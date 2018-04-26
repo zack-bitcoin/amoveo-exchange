5034ae7ac4ee2fb251842b1a1ffdd011c041b884
zack <zack.bitcoin@gmail.com>
2018-04-25 20:50:57 -0400

    made a shell script for deleting the database files.

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#this deletes all the database files
+
+# WARNING, you should change the pubkey for the account in your amoveo full node before restarting the exchange.
+
+rm _build/prod/rel/amoveo_exchange/*.db
